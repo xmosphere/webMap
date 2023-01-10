819ef6db1d286faca2faa1b17d5b1386f455752b
xmosphere <58228974+xmosphere@users.noreply.github.com>
2023-01-09 22:27:32 -0500

    Add files via upload

---
+++ server/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // url: "mongodb://192.168.86.66:27777"
+    //url: "mongodb://0.0.0.0:27017"
+    url: "mongodb+srv://testCluster:CbuiduPKkhjYBHzM@cluster0.ruo0nog.mongodb.net/myFirstDatabase"
+}
