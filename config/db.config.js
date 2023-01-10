19f1164ac83ba6bd6ce232d2dc763635f196d6e8
xmosphere <58228974+xmosphere@users.noreply.github.com>
2023-01-09 22:26:23 -0500

    Delete config directory

--- config/db.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // url: "mongodb://192.168.86.66:27777"
-    //url: "mongodb://0.0.0.0:27017"
-    url: "mongodb+srv://testCluster:CbuiduPKkhjYBHzM@cluster0.ruo0nog.mongodb.net/myFirstDatabase"
-}
